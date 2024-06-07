45e068923669fd6d87687179ab76264a9a785615
coordova <grcordova@gmail.com>
2024-06-07 19:09:19 -0400

    photography-portfolio - Router & Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React from "react";
+
+// import components
+import Header from "./components/Header";
+import AnimRoutes from "./components/AnimRoutes";
+
+// immport router
+import { BrowserRouter as Router } from "react-router-dom";
+
+// import motion
+import { motion } from "framer-motion";
 
 const App = () => {
-  return <div>react app</div>;
+	return (
+		<>
+			<Router>
+				<Header />
+				<AnimRoutes />
+			</Router>
+		</>
+	);
 };
 
 export default App;
