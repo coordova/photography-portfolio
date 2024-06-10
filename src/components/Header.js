e6c172fe17fa06a7d6b908af7f0357d4f7325767
coordova <grcordova@gmail.com>
2024-06-10 18:18:05 -0400

    photography-portfolio - Socials

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
 				</Link>
 
 				{/* nav - initially hidden - show on desktop mode */}
-				<nav className="hidden lg:flex gap-x-12 font-semibold">
+				<nav className="hidden xl:flex gap-x-12 font-semibold">
 					<Link
 						to="/"
 						className="text-[#696c6d hover:text-primary transition"
