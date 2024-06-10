221948cb44f0b58493a1030dc8b868fecce30bf1
coordova <grcordova@gmail.com>
2024-06-10 18:50:41 -0400

    photography-portfolio - Home page styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React from "react";
+
+// import images
+import WomanImg from "../img/home/woman.png";
+
+// import link
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+	return (
+		<section className="section">
+			<div className="container mx-auto h-full relative">
+				{/* text & img wrapper */}
+				<div className="flex flex-col justify-center">
+					{/* text */}
+					<div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
+						<h1 className="h1">
+							Photographer <br /> & Filmmaker
+						</h1>
+
+						<p className="text-[26px] lg:text-[36px] font-primary mb-4 lg:mb-12">
+							Los Angeles, USA
+						</p>
+
+						<Link to={"/contact"} className="btn mb-[30px]">
+							Hire me
+						</Link>
+					</div>
+
+					{/* img */}
+					<div className="flex justify-end">
+						<img src={WomanImg} alt="woman" />
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default Home;
