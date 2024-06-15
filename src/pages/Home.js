7c7530bba9c1d7e1a6bf9b59e22ae52128221da6
coordova <grcordova@gmail.com>
2024-06-14 22:43:02 -0400

    photography-portfolio - Home Page Transitions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,14 +6,32 @@ import WomanImg from "../img/home/woman.png";
 // import link
 import { Link } from "react-router-dom";
 
+// import motion
+import { motion } from "framer-motion";
+
+// import transition
+import { transition, transition1 } from "../transitions";
+
 const Home = () => {
 	return (
-		<section className="section">
+		<motion.section
+			initial={{ opacity: 0 }}
+			animate={{ opacity: 1 }}
+			exit={{ opacity: 0 }}
+			transition={transition1}
+			className="section"
+		>
 			<div className="container mx-auto h-full relative">
 				{/* text & img wrapper */}
 				<div className="flex flex-col justify-center">
 					{/* text */}
-					<div className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start">
+					<motion.div
+						initial={{ opacity: 0, y: "-50%" }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0, y: "-50%" }}
+						transition={transition1}
+						className="w-full pt-36 pb-14 lg:pt-0 lg:pb-0 lg:w-auto z-10 lg:absolute flex flex-col justify-center items-center lg:items-start"
+					>
 						<h1 className="h1">
 							Photographer <br /> & Filmmaker
 						</h1>
@@ -25,17 +43,28 @@ const Home = () => {
 						<Link to={"/contact"} className="btn mb-[30px]">
 							Hire me
 						</Link>
-					</div>
+					</motion.div>
 
 					{/* img */}
 					<div className="flex justify-end max-h-96  lg:max-h-max">
-						<div className="relative lg:-right-40 overflow-hidden">
-							<img src={WomanImg} alt="woman" />
-						</div>
+						<motion.div
+							initial={{ scale: 0 }}
+							animate={{ scale: 1 }}
+							exit={{ scale: 0 }}
+							transition={transition1}
+							className="relative lg:-right-40 overflow-hidden"
+						>
+							<motion.img
+								whileHover={{ scale: 1.1 }}
+								transition={transition1}
+								src={WomanImg}
+								alt="woman"
+							/>
+						</motion.div>
 					</div>
 				</div>
 			</div>
-		</section>
+		</motion.section>
 	);
 };
 
