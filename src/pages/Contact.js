915d08182fba2324e2878aa9d794f4f6ab30d717
coordova <grcordova@gmail.com>
2024-06-14 23:16:34 -0400

    photography-portfolio - Contact Page Transitions

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,13 +3,31 @@ import React from "react";
 // import images
 import WomanImg from "../img/contact/woman.png";
 
+// import motion
+import { motion } from "framer-motion";
+
+// import transition
+import { transition1 } from "../transitions";
+
 const Contact = () => {
 	return (
-		<section className="section bg-white">
+		<motion.section
+			initial={{ opacity: 0, y: "100%" }}
+			animate={{ opacity: 1, y: 0 }}
+			exit={{ opacity: 0, y: "100%" }}
+			transition={transition1}
+			className="section bg-white"
+		>
 			<div className="container mx-auto h-full">
 				<div className="flex flex-col lg:flex-row h-full items-center justify-center pt-36 gap-x-8 text-center lg:text-left">
 					{/* bg */}
-					<div className="hidden lg:flex bg-[#eef7f9] absolute left-0 bottom-0 right-0 top-72 -z-10"></div>
+					<motion.div
+						initial={{ opacity: 0, y: "100%" }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0, y: "100%" }}
+						transition={transition1}
+						className="hidden lg:flex bg-[#eef7f9] absolute left-0 bottom-0 right-0 top-72 -z-10"
+					></motion.div>
 
 					{/* text & form */}
 					<div className="lg:flex-1 lg:pt-32 px-4">
@@ -48,12 +66,18 @@ const Contact = () => {
 					</div>
 
 					{/* image */}
-					<div className="lg:flex-1">
+					<motion.div
+						initial={{ opacity: 0, y: "100%" }}
+						animate={{ opacity: 1, y: 0 }}
+						exit={{ opacity: 0, y: "100%" }}
+						transition={{ transition: transition1, duration: 1.5 }}
+						className="lg:flex-1"
+					>
 						<img src={WomanImg} alt="" />
-					</div>
+					</motion.div>
 				</div>
 			</div>
-		</section>
+		</motion.section>
 	);
 };
 
