a2dbb9bf742712313736fdacbe6ab16489069d2f
coordova <grcordova@gmail.com>
2024-06-24 21:08:55 -0400

    photography-portfolio - Custom Cursor / Cursor context - to Header Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import Socials from "./Socials";
 import MobileNav from "./MobileNav";
@@ -7,12 +7,22 @@ import Logo from "../img/header/logo.svg";
 // import link
 import { Link } from "react-router-dom";
 
+// import cursor context
+import { CursorContext } from "../context/CursorContext";
+
 const Header = () => {
+	const { handleMouseEnter, handleMouseLeave } = useContext(CursorContext);
+
 	return (
 		<header className="fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
 			<div className="flex flex-col lg:flex-row justify-between w-full lg:items-center">
 				{/* logo */}
-				<Link to="/" className="max-w-[200px]">
+				<Link
+					onMouseEnter={handleMouseEnter}
+					onMouseLeave={handleMouseLeave}
+					to="/"
+					className="max-w-[200px]"
+				>
 					<img
 						src={Logo}
 						alt="logo"
@@ -21,34 +31,29 @@ const Header = () => {
 				</Link>
 
 				{/* nav - initially hidden - show on desktop mode */}
-				<nav className="hidden xl:flex gap-x-12 font-semibold">
+				<nav
+					onMouseEnter={handleMouseEnter}
+					onMouseLeave={handleMouseLeave}
+					className="hidden xl:flex gap-x-12 font-semibold"
+				>
 					<Link
 						to="/"
 						className="text-[#696c6d hover:text-primary transition"
 					>
 						Home
 					</Link>
-				</nav>
-
-				<nav className="hidden lg:flex">
 					<Link
 						to="/about"
 						className="text-[#696c6d hover:text-primary transition"
 					>
 						About
 					</Link>
-				</nav>
-
-				<nav className="hidden lg:flex">
 					<Link
 						to="/portfolio"
 						className="text-[#696c6d hover:text-primary transition"
 					>
 						Portfolio
 					</Link>
-				</nav>
-
-				<nav className="hidden lg:flex">
 					<Link
 						to="/contact"
 						className="text-[#696c6d hover:text-primary transition"
